136c4f2f2af375adddee3475633b940de4045a3e
xuquanru <1163990163@qq.com>
2021-08-02 20:07:18 +0800

    测试maven

---
+++ src/main/java/io/github/fairyspace/coolutils/C.java
@@ -0,0 +1,225 @@
+package io.github.fairyspace.coolutils;
+
+public class C {
+    public static String Red(String color) {
+        return Color.RED + color + Color.RESET;
+    }
+
+    public static String Green(String color) {
+        return Color.GREEN + color + Color.RESET;
+    }
+
+    public static String White(String color) {
+        return Color.WHITE + color + Color.RESET;
+    }
+
+    public static String Yellow(String color) {
+        return Color.YELLOW + color + Color.RESET;
+    }
+
+    public static String Blue(String color) {
+        return Color.BLUE + color + Color.RESET;
+    }
+
+    public static String Purple(String color) {
+        return Color.PURPLE + color + Color.RESET;
+    }
+
+    public static String Cyan(String color) {
+        return Color.CYAN + color + Color.RESET;
+    }
+
+    public static String Grey(String color) {
+        return Color.GREY + color + Color.RESET;
+    }
+
+    public static String WhiteBold(String color) {
+        return Color.WHITE_BOLD + color + Color.RESET;
+    }
+
+    public static String RedBold(String color) {
+        return Color.RED_BOLD + color + Color.RESET;
+    }
+
+    public static String GreenBold(String color) {
+        return Color.GREEN_BOLD + color + Color.RESET;
+    }
+
+    public static String YellowBold(String color) {
+        return Color.YELLOW_BOLD + color + Color.RESET;
+    }
+
+    public static String BlueBold(String color) {
+        return Color.BLUE_BOLD + color + Color.RESET;
+    }
+
+    public static String PurpleBold(String color) {
+        return Color.PURPLE_BOLD + color + Color.RESET;
+    }
+
+    public static String CyanBold(String color) {
+        return Color.CYAN_BOLD + color + Color.RESET;
+    }
+
+    public static String GreyBold(String color) {
+        return Color.GREY_BOLD + color + Color.RESET;
+    }
+
+
+    public static String WhiteUnderlined(String color) {
+        return Color.WHITE_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String RedUnderlined(String color) {
+        return Color.RED_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String YellowUnderlined(String color) {
+        return Color.YELLOW_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String GreenUnderlined(String color) {
+        return Color.GREEN_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String BlueUnderlined(String color) {
+        return Color.BLUE_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String PurpleUnderlined(String color) {
+        return Color.PURPLE_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String CyanUnderlined(String color) {
+        return Color.CYAN_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String GreyUnderlined(String color) {
+        return Color.GREY_UNDERLINED + color + Color.RESET;
+    }
+
+    public static String WhiteBackground(String color) {
+        return Color.WHITE_BACKGROUND + color + Color.RESET;
+    }
+
+    public static String RedBackground(String color) {
+        return Color.RED_BACKGROUND + color + Color.RESET;
+    }
+
+    public static String GreenBackground(String color) {
+        return Color.GREEN_BACKGROUND + color + Color.RESET;
+    }
+
+    public static String YellowBackground(String color) {
+        return Color.YELLOW_BACKGROUND + color + Color.RESET;
+    }
+
+    public static String BlueBackground(String color) {
+        return Color.BLUE_BACKGROUND + color + Color.RESET;
+    }
+
+    public static String PurpleBackground(String color) {
+        return Color.PURPLE_BACKGROUND + color + Color.RESET;
+    }
+
+    public static String GreyBackground(String color) {
+        return Color.GREY_BACKGROUND + color + Color.RESET;
+    }
+
+    public static String WhiteBright(String color) {
+        return Color.WHITE_BRIGHT + color + Color.RESET;
+    }
+
+    public static String RedBright(String color) {
+        return Color.RED_BRIGHT + color + Color.RESET;
+    }
+
+    public static String GreenBright(String color) {
+        return Color.GREEN_BRIGHT + color + Color.RESET;
+    }
+
+    public static String YellowBright(String color) {
+        return Color.YELLOW_BRIGHT + color + Color.RESET;
+    }
+
+    public static String BlueBright(String color) {
+        return Color.BLUE_BRIGHT + color + Color.RESET;
+    }
+
+    public static String PurpleBright(String color) {
+        return Color.PURPLE_BRIGHT + color + Color.RESET;
+    }
+
+    public static String CyanBright(String color) {
+        return Color.CYAN_BRIGHT + color + Color.RESET;
+    }
+
+    public static String GreyBright(String color) {
+        return Color.GREY_BRIGHT + color + Color.RESET;
+    }
+
+    public static String WhiteBoldBright(String color) {
+        return Color.WHITE_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+    public static String RedBoldBright(String color) {
+        return Color.RED_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+    public static String GreenBoldBright(String color) {
+        return Color.GREEN_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+    public static String YellowBoldBright(String color) {
+        return Color.YELLOW_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+    public static String BlueBoldBright(String color) {
+        return Color.BLUE_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+
+    public static String PurpleBoldBright(String color) {
+        return Color.PURPLE_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+    public static String CyanBoldBright(String color) {
+        return Color.CYAN_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+    public static String GreyBoldBright(String color) {
+        return Color.GREY_BOLD_BRIGHT + color + Color.RESET;
+    }
+
+    public static String WhiteBackgroundBright(String color) {
+        return Color.WHITE_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+
+    public static String RedBackgroundBright(String color) {
+        return Color.RED_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+
+    public static String GreenBackgroundBright(String color) {
+        return Color.GREEN_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+
+    public static String YellowBackgroundBright(String color) {
+        return Color.YELLOW_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+
+    public static String BlueBackgroundBright(String color) {
+        return Color.BLUE_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+
+    public static String PurpleBackgroundBright(String color) {
+        return Color.PURPLE_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+
+    public static String CyanBackgroundBright(String color) {
+        return Color.CYAN_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+
+    public static String GreyBackgroundBright(String color) {
+        return Color.GREY_BACKGROUND_BRIGHT + color + Color.RESET;
+    }
+}
