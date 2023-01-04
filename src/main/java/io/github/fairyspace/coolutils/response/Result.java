3502b6cae82076902331a74cc92e5843883c86ef
xuquanru <1163990163@qq.com>
2023-01-04 17:58:09 +0800

    新增多个功能2

--- src/main/java/io/github/fairyspace/coolutils/response/Result.java
+++ src/main/java/io/github/fairyspace/coolutils/response/Result.java
@@ -1,18 +1,11 @@
 package io.github.fairyspace.coolutils.response;
 
-/**
- * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌道阻且长，行则将至🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
- * 🍁 Program: coolutils
- * 🍁 Description:
- * 🍁 @author: xuquanru
- * 🍁 Create: 2022/4/18
- * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌行而不辍，未来可期🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
- **/
+
 public class Result<T> {
     private Integer code;
-    /** 信息描述 */
+
     private String message;
-    /** 返回参数 */
+
     private T data;
 
     private Result(ResultStatus resultStatus, T data) {
@@ -27,23 +20,23 @@ public class Result<T> {
         this.data = data;
     }
 
-    /** 业务成功返回业务代码和描述信息 */
+
     public static <T> Result<T> success(Integer code, String message, T data) {
         return new Result<T>(code,message, data);
     }
 
 
-    /** 业务成功返回业务代码和描述信息 */
+
     public static Result<Void> success() {
         return new Result<Void>(ResultStatus.SUCCESS, null);
     }
 
-    /** 业务成功返回业务代码,描述和返回的参数 */
+
     public static <T> Result<T> success(T data) {
         return new Result<T>(ResultStatus.SUCCESS, data);
     }
 
-    /** 业务成功返回业务代码,描述和返回的参数 */
+
     public static <T> Result<T> success(ResultStatus resultStatus, T data) {
         if (resultStatus == null) {
             return success(data);
@@ -51,23 +44,23 @@ public class Result<T> {
         return new Result<T>(resultStatus, data);
     }
 
-    /** 业务异常返回业务代码和描述信息 */
+
     public static <T> Result<T> failure(Integer code,String message,T data) {
         return new Result<T>(code,message, data);
     }
 
 
-    /** 业务异常返回业务代码和描述信息 */
+
     public static <T> Result<T> failure() {
         return new Result<T>(ResultStatus.INTERNAL_SERVER_ERROR, null);
     }
 
-    /** 业务异常返回业务代码,描述和返回的参数 */
+
     public static <T> Result<T> failure(ResultStatus resultStatus) {
         return failure(resultStatus, null);
     }
 
-    /** 业务异常返回业务代码,描述和返回的参数 */
+
     public static <T> Result<T> failure(ResultStatus resultStatus, T data) {
         if (resultStatus == null) {
             return new Result<T>(ResultStatus.INTERNAL_SERVER_ERROR, null);
